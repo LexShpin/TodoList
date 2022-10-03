35f9f680459ce68ad66eba6887a44d34288d8eda
lexshpin <lexshpin@gmail.com>
2022-10-03 13:21:29 -0600

    Finished the tests and the logic for the TodoList class

--- src/main/java/org/example/TodoList.java
+++ src/main/java/org/example/TodoList.java
@@ -4,12 +4,51 @@ import java.util.ArrayList;
 
 public class TodoList {
     private ArrayList<String> todos;
+    private ArrayList<String> completedTodos;
 
     public TodoList() {
         this.todos = new ArrayList<>();
+        this.completedTodos = new ArrayList<>();
     }
 
     public void add(String todo) {
+        this.todos.add(todo);
+    }
 
+    public void remove(int number) {
+        this.todos.remove(number - 1);
+    }
+
+    public void updateTodo(int number, String text) {
+        this.todos.set(number - 1, text);
+    }
+
+    public void printAll() {
+        for (int i = 0; i < this.todos.size(); i++) {
+            System.out.println((i + 1) + ": " + this.todos.get(i));
+        }
+    }
+
+    public String getTodo(int number) {
+        return this.todos.get(number - 1);
+    }
+
+    public int getTodoSize() {
+        return this.todos.size();
+    }
+
+    public void completeTodo(int number) {
+        this.completedTodos.add(this.todos.get(number));
+        this.todos.remove(number);
+    }
+
+    public int getCompletedSize() {
+        return this.completedTodos.size();
+    }
+
+    public void printCompleted() {
+        for (int i = 0; i < this.completedTodos.size(); i++) {
+            System.out.println((i + 1) + ": " + this.completedTodos.get(i));
+        }
     }
 }
--- src/test/java/org/example/TodoListTest.java
+++ src/test/java/org/example/TodoListTest.java
@@ -1,9 +1,41 @@
 package org.example;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TodoListTest {
 
-//    @Test
-//    public void
+    TodoList todoList = new TodoList();
+
+    @Test
+    public void todoGetsAdded() {
+        int previousSize = this.todoList.getTodoSize();
+        todoList.add("some todo");
+        assertEquals(previousSize + 1, this.todoList.getTodoSize());
+    }
+
+    @Test
+    public void todoGetsRemoved() {
+        todoList.add("some todo");
+        int previousSize = this.todoList.getTodoSize();
+        todoList.remove(1);
+        assertEquals(previousSize - 1, this.todoList.getTodoSize());
+    }
+
+    @Test
+    public void todoGetsCompleted() {
+        int previousSizeCompleted = this.todoList.getCompletedSize();
+        todoList.add("new todo");
+        int previousSizeTodos = this.todoList.getTodoSize();
+        todoList.completeTodo(0);
+        assertEquals(previousSizeTodos - 1, this.todoList.getTodoSize());
+        assertEquals(previousSizeCompleted + 1, this.todoList.getCompletedSize());
+    }
+
+    @Test
+    public void todoGetsUpdated() {
+        todoList.add("new todo");
+        todoList.updateTodo(1, "buy milk");
+        assertEquals("buy milk", todoList.getTodo(1));
+    }
 }
